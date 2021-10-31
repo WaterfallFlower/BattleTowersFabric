6053795428909a7a93216586a5648cf34be4c60c
WaterfallFlower <alimhangmg99@gmail.com>
2021-10-31 14:34:11 +0600

    Preparing workspace.

---
+++ src/main/java/net/waterfallflower/battletowers/InitializationClass.java
@@ -0,0 +1,14 @@
+package net.waterfallflower.battletowers;
+
+import net.mine_diver.unsafeevents.listener.EventListener;
+import net.modificationstation.stationapi.api.event.registry.BlockRegistryEvent;
+import net.modificationstation.stationapi.api.template.block.TemplateBlockBase;
+
+public class InitializationClass {
+
+    @EventListener
+    public void registerBlocks(BlockRegistryEvent event) {
+
+    }
+
+}
